81d15d382974bfa5672e7a5c707f06ebffa0fe8e
jkbo <lyngknuden@gmail.com>
2024-10-07 12:44:16 +0000

    digest on the item

--- aapmgr/inventory/models.py
+++ aapmgr/inventory/models.py
@@ -84,7 +84,6 @@ class server(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField()
     appid = models.ForeignKey(appid, on_delete=models.CASCADE)
-    organization = models.ForeignKey(organization, on_delete=models.CASCADE)
     environment = models.ForeignKey(environment, on_delete=models.CASCADE)
     region = models.ForeignKey(region, on_delete=models.CASCADE)
     zone = models.ForeignKey(zone, on_delete=models.CASCADE)
