61715c9ea4711fe9c54f1a3f1f369343995d539e
jkbo <lyngknuden@gmail.com>
2024-10-09 12:27:12 +0000

    demo

--- backend/inventory/subnets.py
+++ backend/inventory/subnets.py
@@ -14,32 +14,37 @@ import pprint
 def main():
     r = redis.Redis(host='localhost', port=6379, db=0)
     subnets = {}
-    print("netcat")
+    dmzserver = {}
+    zone = {}
+
     keys = r.keys("serverinfo:netcat:*")
     for key in keys:
         server = key.decode("utf-8").split(":")[2]
         value = r.get(key).decode("utf-8")
-
         if value == "0":
-            print(server, "ssh port is open")
             dig = pydig.query(server, 'A')
-            print(dig)
             for ip in dig:
-                print(ip)
                 try:
                     socket.inet_aton(ip)
-                    print("valid ip")
                     # replace last octet with 0
                     ip = ip.rsplit(".", 1)[0] + ".0"
                     splitmyserver = server.split(".")
                     pprint.pprint(splitmyserver)
                     subnets[ip] = splitmyserver[1]
-                    
+                    try:
+                        myzone = zone['splitmyserver[1]'] 
+                    except KeyError:
+                        zone['splitmyserver[1]'] = []
+
+                    zone['splitmyserver[1]'].append(server)
+                                                   
+
                 except socket.error:
                     print("invalid ip")
 
     
     print("subnets", subnets)
+    print("zone", zone)
     
 
 
