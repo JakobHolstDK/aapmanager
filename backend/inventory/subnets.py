fcc80cf2063b286dc8bfedfd03036dfc917c6d91
jkbo <lyngknuden@gmail.com>
2024-10-09 07:18:33 +0000

    subnet cababilities

--- backend/inventory/subnets.py
+++ backend/inventory/subnets.py
@@ -3,10 +3,20 @@ import redis
 
 def main():
     r = redis.Redis(host='localhost', port=6379, db=0)
-    keys = r.keys("*")
+    keys = r.keys("serverinfo:netcat:*")
     for key in keys:
-        print(key)
-        print(r.get(key))
+        server = key.split(":")[2]
+        print(server)
+
+    keys = r.keys("serverinfo:dig:*")
+    for key in keys:
+        server = key.split(":")[2]
+        print(server)
+        
+
+    
+
+
 
 
 if __name__ == '__main__':
